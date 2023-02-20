79c6dfc07954b2772cd2e6dc7c034ab4778007a2
vadim <vadimpoldolin19@gmail.com>
2023-02-20 20:37:18 +0300

    Задача 5-7

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -30,5 +36,32 @@ public class Main {
      for(int v=10;v>=-10;v=v-1){
          System.out.println(v);
      }
+    }
+    public static void task5(){
+        System.out.println("Задача 5");
+        for(int i=1904;i<2096;i=i+4){
+            System.out.println(i + " год является високосным");
+        }
+    }
+    public static void task6(){
+        System.out.println("Задачв 6");
+        for(int c=7;c<=98;c=c+7){
+            System.out.println(c);
+        }
+    }
+    public static void task7(){
+        System.out.println("Задачв 7");
+        for(int m=2;m<=512;m=m*2){
+            System.out.println(m);
+        }
+}
+    public static void task8() {
+        System.out.println("Задача 8");
+    }
+    public static void task9() {
+        System.out.println("Задача 9");
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
     }
 }
